23d6f3f9cb90eb25fffba4efd3e78b5f3501978d
Hyowon Kim <80298648+hourglass@users.noreply.github.com>
2025-06-02 15:03:36 +0900

    MouseDirection 수정

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,72 +7,12 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject spriteObject;
-
-    [SerializeField]
-    private float moveSpeed;
-
-
-    [SerializeField]
-    private float dashForce;
-
-    [SerializeField]
-    private float dashDrag;
-
-    [SerializeField]
-    private float dashTime;
-
-    [SerializeField]
-    private float dashDelay;
-
-
-    [SerializeField]
-    private float attackForce;
-
-    [SerializeField]
-    private float attackDrag;
-
-    [SerializeField]
-    private float attackTime;
-
-    [SerializeField]
-    private float attackDelay;
-
-
-    private enum PlayerState
-    {
-        Idle,
-        Move,
-        Dash,
-        Attack
-    }
-
-    private PlayerState currentState;
-
-    private PlayerInput playerControls;
-    private Rigidbody2D rb;
-    private Vector2 movement;
-    private Vector3 mouseDirection;
-
-    private Animator myAnimator;
-    private SpriteRenderer mySpriteRenderer;
-
-    Coroutine dashCorutine;
-    private bool dashEnabled;
-
-    Coroutine attackCorutine;
-    private bool attackEnabled;
-    private int maxComboIndex;
-    private int currentComboIndex;
-
-
     private void Awake()
     {
         playerControls = new PlayerInput();
         rb = GetComponent<Rigidbody2D>();
-        myAnimator = spriteObject.GetComponent<Animator>();
-        mySpriteRenderer = spriteObject.gameObject.GetComponent<SpriteRenderer>();
+        playerAnim = PlayerSprite.GetComponent<Animator>();
+        playerSpriteRenderer = PlayerSprite.gameObject.GetComponent<SpriteRenderer>();
 
         maxComboIndex = 1;
         currentComboIndex = 0;
@@ -121,38 +61,43 @@ public class PlayerController : MonoBehaviour
             rb.velocity = movement * moveSpeed * Time.deltaTime;
 
             // 스프라이트 플립
-            mySpriteRenderer.flipX = (movement.x < 0f) ? true : false;
+            playerSpriteRenderer.flipX = (movement.x < 0f) ? true : false;
 
             // 값을 애니메이터에 적용
-            myAnimator.SetFloat("InputX", movement.x);
-            myAnimator.SetFloat("InputY", movement.y);
+            playerAnim.SetFloat("InputX", movement.x);
+            playerAnim.SetFloat("InputY", movement.y);
         }
     }
 
     //==============================[ Mouse ]==============================//
-    private void ConvertMousePosToBlend()
+    private Vector3 GetMouseDirection()
     {
         // 마우스와 플레이어의 스크린 좌표 가져오기
         Vector3 mouseScreenPoint = Input.mousePosition;
         Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
 
         // 벡터 정규화
-        mouseDirection = mouseScreenPoint - playerScreenPoint;
+        Vector3 mouseDirection = mouseScreenPoint - playerScreenPoint;
         mouseDirection.Normalize();
 
+        return mouseDirection;
+    }
+
+    private void SetAnimByMouseDirection(Vector3 mouseDirection)
+    {
         // 벡터의 값을 -1 ~ 1 범위로 변환
         float mouseX = Vector3.Dot(transform.right, mouseDirection);
         float mouseY = Vector3.Dot(transform.up, mouseDirection);
 
         // 스프라이트 플립  
-        mySpriteRenderer.flipX = (mouseX < 0) ? true : false;
+        playerSpriteRenderer.flipX = (mouseX < 0) ? true : false;
 
         // 값을 애니메이터에 적용
-        myAnimator.SetFloat("MouseX", mouseX);
-        myAnimator.SetFloat("MouseY", mouseY);
+        playerAnim.SetFloat("MouseX", mouseX);
+        playerAnim.SetFloat("MouseY", mouseY);
 
-        myAnimator.SetFloat("InputX", mouseX);
-        myAnimator.SetFloat("InputY", mouseY);
+        playerAnim.SetFloat("InputX", mouseX);
+        playerAnim.SetFloat("InputY", mouseY);
     }
 
     //==============================[ Dash ]==============================//
@@ -167,21 +112,25 @@ public class PlayerController : MonoBehaviour
         {
             StopCoroutine(attackCorutine);
         }
+       
+        Vector3 mouseDirection = GetMouseDirection();
+        SetAnimByMouseDirection(mouseDirection);
 
         ChangeState(PlayerState.Dash);
-        ConvertMousePosToBlend();
+
         dashCorutine = StartCoroutine(DashCoroutine());
     }
 
     private IEnumerator DashCoroutine()
     {
+        Vector3 mouseDirection = GetMouseDirection();
         rb.AddForce(mouseDirection * dashForce, ForceMode2D.Impulse);
 
         yield return new WaitForSeconds(dashTime);
 
         rb.velocity = Vector3.zero;
         attackEnabled = true;
-        myAnimator.SetInteger("State", (int)PlayerState.Idle);
+        playerAnim.SetInteger("State", (int)PlayerState.Idle);
 
         yield return new WaitForSeconds(dashDelay);
 
@@ -201,8 +150,13 @@ public class PlayerController : MonoBehaviour
             StopCoroutine(dashCorutine);
         }
 
+        Vector3 mouseDirection = GetMouseDirection();
+        SetAnimByMouseDirection(mouseDirection);
+
         ChangeState(PlayerState.Attack);
-        ConvertMousePosToBlend();
+
+        playerAnim.SetInteger("ComboIndex", currentComboIndex);
+
         attackCorutine = StartCoroutine(AttackCoroutine());
     }
 
@@ -226,9 +180,8 @@ public class PlayerController : MonoBehaviour
 
     private void Attack()
     {
+        Vector3 mouseDirection = GetMouseDirection();
         rb.AddForce(mouseDirection * attackForce, ForceMode2D.Impulse);
-
-        myAnimator.SetInteger("ComboIndex", currentComboIndex);
 
         currentComboIndex++;
         if (currentComboIndex > maxComboIndex)
@@ -267,8 +220,67 @@ public class PlayerController : MonoBehaviour
                 break;
         }
 
-        myAnimator.SetInteger("State", (int)state);
+        playerAnim.SetInteger("State", (int)state);
         currentState = state;
     }
+
+
+    [SerializeField]
+    private GameObject PlayerSprite;
+
+    [SerializeField]
+    private float moveSpeed;
+
+    [SerializeField]
+    private float dashForce;
+
+    [SerializeField]
+    private float dashDrag;
+
+    [SerializeField]
+    private float dashTime;
+
+    [SerializeField]
+    private float dashDelay;
+
+    [SerializeField]
+    private float attackForce;
+
+    [SerializeField]
+    private float attackDrag;
+
+    [SerializeField]
+    private float attackTime;
+
+    [SerializeField]
+    private float attackDelay;
+
+
+    private enum PlayerState
+    {
+        Idle,
+        Move,
+        Dash,
+        Attack
+    }
+
+    private PlayerState currentState;
+
+    private PlayerInput playerControls;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    private Animator playerAnim;
+    private SpriteRenderer playerSpriteRenderer;
+
+    Coroutine dashCorutine;
+    private bool dashEnabled;
+
+    Coroutine attackCorutine;
+    private bool attackEnabled;
+    private int maxComboIndex;
+    private int currentComboIndex;
+
+    Weapon currentWeapon;
 }
 
